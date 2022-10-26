1d52209a0fc752b9e70703789b78c76240436d59
Diegoeverton <diegoeverton.py@gmail.com>
2022-10-26 20:53:42 -0300

    pythondjangoaula12

--- viagens/forms.py
+++ viagens/forms.py
@@ -1,8 +1,51 @@
+from importlib.metadata import files
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from datetime import datetime
+from viagens.classe_viagem import tipos_de_classe
+from viagens.validar import *
+from viagens.models import Viagem, ClasseViagem, Pessoa
 
-class ViagemForms(forms.Form):
-    origem = forms.CharField(label='Origem', max_length=100)
-    destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label='Ida', widget=DatePicker())
-    data_volta = forms.DateField(label='Volta', widget=DatePicker())
+
+
+class ViagemForms(forms.ModelForm):
+    data_pesquisa = forms.DateField(label='Data da pesquisa',disabled=True, initial=datetime.today)
+
+    class Meta:
+        model = Viagem
+        fields = '__all__'
+        labels = {'data_ida':'Data de ida', 'data_volta': 'Data de volta', 'informacoes':'informacoes', 'classe_viagem':'Tipo de voo'}
+        widgets = {
+            'data_ida': DatePicker(),
+            'data_volta': DatePicker()
+        }
+
+class PessoaForms(forms.ModelForm):
+    class Meta:
+        model = Pessoa
+        exclude = ['nome']
+
+
+   
+    
+   
+   
+
+    def clean(self):
+        origem = self.cleaned_data.get('origem')
+        destino = self.cleaned_data.get('destino')
+        data_ida = self.cleaned_data.get('data_ida')
+        data_volta = self.cleaned_data.get('data_volta')
+        data_pesquisa = self.cleaned_data.get('Data_pesquisa')
+        
+        listaErros = {}
+        campo_temNumero(origem, 'origem', listaErros)
+        campo_temNumero(destino, 'destino', listaErros)
+        origem_destino_iguais(origem, destino, listaErros)
+        if listaErros is not None:
+            for erro in listaErros:
+                mensagem_erro = listaErros[erro]
+                self.add_error(erro, mensagem_erro)
+        return self.cleaned_data
+    
+
--- viagens/views.py
+++ viagens/views.py
@@ -1,18 +1,24 @@
 from http.client import FORBIDDEN
-import re
 from django.shortcuts import render
-from viagens.forms import ViagemForms
+from viagens.forms import ViagemForms, PessoaForms
 
 
 # Create your views here.
 def index(request):
     form= ViagemForms()
-    contexto = {'form' : form}
+    pessoa_form = PessoaForms()
+    contexto = {'form' : form, 'pessoa_form':pessoa_form}
     return render(request, 'index.html', contexto)
 
 def revConsulta(request):
     if request.method == 'POST':
         form = ViagemForms(request.POST)
-        contexto={'form' : form}
-        return render (request, 'consulta.html', contexto)
+        pessoa_form = PessoaForms(request.POST)
+        if form.is_valid():
+            contexto={'form' : form, 'pessoa_form': pessoa_form}
+            return render(request,  'consulta.html',contexto)
+        else:
+            print('Form inválido')
+            contexto={'form' : form, 'pessoa_form':pessoa_form}
+            return render (request, 'consulta.html', contexto)
 
